9b23d1b2b52aad35fef8a82d6ea07a1aab6fd775
Raaga Krishna <raaga@bbd.co.za>
2023-02-22 11:13:55 +0200

    Delete and Drop tables script

---
+++ DROP_TABLES.sql
@@ -0,0 +1,70 @@
+-- DELETE ALL TABLES
+USE BudgetTrackingApp;
+GO
+
+
+-- Delete Transaction table
+DELETE FROM [dbo].[Transaction];
+GO
+
+DBCC CHECKIDENT ('Transaction', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Transaction];
+GO
+
+
+-- Delete Income
+DELETE FROM [dbo].[Income];
+GO
+
+DBCC CHECKIDENT ('Income', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Income];
+GO
+
+
+
+-- Delete Expense
+DELETE FROM [dbo].[Expense];
+GO
+
+DBCC CHECKIDENT ('Expense', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Expense];
+GO
+
+
+-- Delete Budget
+DELETE FROM [dbo].[Budget];
+GO
+
+DBCC CHECKIDENT ('Budget', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Budget];
+GO
+
+
+-- Delete User
+DELETE FROM [dbo].[User];
+GO
+
+DBCC CHECKIDENT ('User', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[User];
+GO
+
+
+-- Delete Tax_Implication
+DELETE FROM [dbo].[Tax_Implication];
+GO
+
+DBCC CHECKIDENT ('Tax_Implication', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Tax_Implication];
+GO
