6ff2f660e4277c35b6b2cd65fd89468b0cda90ce
Raaga Krishna <raaga@bbd.co.za>
2023-02-23 13:07:58 +0200

    Create and Drop scripts

---
+++ CREATE_TABLES.sql
@@ -0,0 +1,84 @@
+--USE master;
+--GO
+
+--CREATE DATABASE BudgetTrackingAppFinal;
+--GO
+
+-- Use the database 
+USE BudgetTrackingAppFinal
+GO
+
+
+CREATE TABLE [User] (
+  [user_id] INT PRIMARY KEY IDENTITY(1, 1),
+  [username] VARCHAR(150),
+  [password] VARCHAR(50),
+  [email] VARCHAR(100),
+  [full_name] VARCHAR(150)
+)
+GO
+
+
+CREATE TABLE [Budget] (
+  [budget_id] INT PRIMARY KEY IDENTITY(1, 1),
+  [fk_user_id] INT NOT NULL,
+  [budget_name] VARCHAR(150),
+  [budget_start_date] DATE,
+  [budget_end_date] DATE,
+  [budget_target_amount] DECIMAL(6,2),
+  [fk_budget_category_id] INT
+)
+GO
+
+ALTER TABLE [Budget] ADD FOREIGN KEY ([fk_user_id]) REFERENCES [User] ([user_id])
+GO
+
+
+CREATE TABLE [Transaction] (
+  [transaction_id] INT PRIMARY KEY IDENTITY(1, 1),
+  [fk_budget_id] INT NOT NULL,
+  [transaction_date] DATE,
+  [transaction_amount] DECIMAL(6,2),
+  [transaction_reference] VARCHAR(150),
+  [transaction_3rdparty] INT
+)
+GO
+
+ALTER TABLE [Transaction] ADD FOREIGN KEY ([fk_budget_id]) REFERENCES [Budget] ([budget_id])
+GO
+
+
+
+CREATE TABLE [Expense_Transaction] (
+  [expense_id] INT PRIMARY KEY IDENTITY(1, 1),
+  [fk_transaction_id] INT,
+  [expense_recipient] VARCHAR(150)
+)
+GO
+
+ALTER TABLE [Expense_Transaction] ADD FOREIGN KEY ([fk_transaction_id]) REFERENCES [Transaction] ([transaction_id])
+GO
+
+
+
+CREATE TABLE [Income_Transaction] (
+  [income_id] INT PRIMARY KEY IDENTITY(1, 1),
+  [fk_transaction_id] INT,
+  [income_source] VARCHAR(80)
+)
+GO
+
+ALTER TABLE [Income_Transaction] ADD FOREIGN KEY ([fk_transaction_id]) REFERENCES [Transaction] ([transaction_id])
+GO
+
+
+
+CREATE TABLE [Budget_Category] (
+  [budget_category_id] INT PRIMARY KEY IDENTITY(1, 1),
+  [budget_category_name] VARCHAR(150),
+  [budget_category_trigger_reference] VARCHAR(150)
+)
+GO
+
+ALTER TABLE [Budget] ADD FOREIGN KEY ([fk_budget_category_id]) REFERENCES [Budget_Category] ([budget_category_id])
+GO
---
+++ DROP_TABLES.sql
@@ -0,0 +1,70 @@
+-- DELETE ALL TABLES
+USE BudgetTrackingAppFinal;
+GO
+
+-- Delete Expense_Transaction table
+DELETE FROM [dbo].[Expense_Transaction]
+GO
+
+DBCC CHECKIDENT ('Expense_Transaction', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Expense_Transaction];
+GO
+
+
+
+-- Delete Income_Transaction table
+DELETE FROM [dbo].[Income_Transaction]
+GO
+
+DBCC CHECKIDENT ('Income_Transaction', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Income_Transaction];
+GO
+
+
+-- Delete Transaction table
+DELETE FROM [dbo].[Transaction];
+GO
+
+DBCC CHECKIDENT ('Transaction', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Transaction];
+GO
+
+
+-- Delete Budget table
+DELETE FROM [dbo].[Budget];
+GO
+
+DBCC CHECKIDENT ('Budget', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Budget];
+GO
+
+
+-- Delete Budget_Category table
+DELETE FROM [dbo].[Budget_Category];
+GO
+
+DBCC CHECKIDENT ('Budget_Category', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[Budget_Category];
+GO
+
+
+-- Delete User table
+DELETE FROM [dbo].[User];
+GO
+
+DBCC CHECKIDENT ('User', RESEED, 0);
+GO
+
+DROP TABLE [dbo].[User];
+GO
+
