bbeecf94d22fb2a93d3d3137cdc8a750bce3d3f6
Raaga Krishna <raaga@bbd.co.za>
2023-02-23 13:15:30 +0200

    Stored proc User

---
+++ TEST_STORED_PROC_ADD_USER.sql
@@ -0,0 +1,26 @@
+USE BudgetTrackingAppFinal;
+GO
+
+
+DECLARE @WhichUser VARCHAR(150);
+SET @WhichUser = 'raagakrishna';
+
+DECLARE @WhichUser2 VARCHAR(150);
+SET @WhichUser2 = 'shagan';
+
+SELECT * FROM [dbo].[User];
+
+-- create a user 
+EXEC [dbo].[AddUser] 
+	@Username = @WhichUser, 
+	@Password = 'hello123', 
+	@Email = 'raaga@gmail.com', 
+	@FullName = 'Raaga Krishna';
+
+EXEC [dbo].[AddUser] 
+	@Username = @WhichUser2, 
+	@Password = 'secret', 
+	@Email = 'shagan@gmail.com', 
+	@FullName = 'Shagan Plaatjies';
+
+SELECT * FROM [dbo].[User];
