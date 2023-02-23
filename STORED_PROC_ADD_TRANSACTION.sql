82252a7fa094b9e618564ae6a6752af720c452c7
Raaga Krishna <raaga@bbd.co.za>
2023-02-23 15:05:48 +0200

    Modified add transaction script

--- STORED_PROC_ADD_TRANSACTION.sql
+++ STORED_PROC_ADD_TRANSACTION.sql
@@ -20,33 +20,19 @@ BEGIN
 	--DECLARE @fk_budget_id INT;
 	--SET @fk_budget_id = (SELECT budget_id FROM [dbo].[Budget] WHERE budget_name = @BudgetName);
 
-	MERGE [Transaction] AS TARGET
-	USING (
-		SELECT --@fk_budget_id as [fk_budget_id]
-				--,
-				@fk_user_id as [fk_user_id]
-				, @TransactionDate as [transaction_date]
-				, @TransactionAmount as [transaction_amount]
-				, @TransactionReference as [transaction_reference]
-				, @TransactionThirdParty as [transaction_3rdparty]
-	)
-	AS SOURCE 
-	ON (--TARGET.fk_budget_id = SOURCE.fk_budget_id
-		--AND 
-		TARGET.fk_user_id = SOURCE.fk_user_id
-		AND TARGET.transaction_reference = SOURCE.transaction_reference
-	)
-	WHEN MATCHED THEN 
-		UPDATE SET [transaction_date] = @TransactionDate
-				, [transaction_amount] = @TransactionAmount
-				, [transaction_3rdparty] = @TransactionThirdParty
-	WHEN NOT MATCHED THEN
-		INSERT (--[fk_budget_id], 
-				[fk_user_id],
-				[transaction_date], [transaction_amount], [transaction_reference], [transaction_3rdparty])
-		VALUES (--@fk_budget_id,
-				@fk_user_id,
-				@TransactionDate, @TransactionAmount, @TransactionReference, @TransactionThirdParty
+	INSERT INTO [Transaction]
+				([fk_user_id]
+				, [transaction_date]
+				, [transaction_amount]
+				, [transaction_reference]
+				, [transaction_3rdparty]
+				)
+	VALUES (@fk_user_id
+			, @TransactionDate
+			, @TransactionAmount
+			, @TransactionReference
+			, @TransactionThirdParty
 	);
+
 END;
 GO
