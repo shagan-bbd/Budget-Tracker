d320cb1a5983ebb2ce431a8ad23eef2527b45fbe
Mmasehume Raphiri <mmasehume@bbd.co.za>
2023-02-24 06:04:26 +0200

    update view

--- Views/MonthlyBudgetView.sql
+++ Views/MonthlyBudgetView.sql
@@ -9,3 +9,8 @@ SELECT budgetName, budgetLimit
 FROM Budget INNER JOIN Periods 
 ON Budget.fk_periodId = Periods.periodId
 WHERE MONTH(Periods.startDate) = 3
+
+
+SELECT [budgetName]
+	,[budgetLimit]
+FROM [BudgetTrackingDB].[dbo].[MonthlyBudget]
