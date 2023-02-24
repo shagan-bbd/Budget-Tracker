d106f77923f74ba0bf2cb32db10f8e77cb53cb11
MichaelC0ker <122515241+MichaelC0ker@users.noreply.github.com>
2023-02-24 05:28:37 +0200

    Update PopulateBudgetSetting.sql

--- Populate/PopulateBudgetSetting.sql
+++ Populate/PopulateBudgetSetting.sql
@@ -2,8 +2,46 @@ USE BudgetTrackingDB;
 GO
 
 EXEC [dbo].[AddBudgetSetting] 
+        @Fk_budgetId = 1,
+		@Fk_userId = 1,
+		@BudgetTrigger = 'Chicken';
+GO
+EXEC [dbo].[AddBudgetSetting] 
+        @Fk_budgetId = 2,
+		@Fk_userId = 1,
+		@BudgetTrigger = 'Mr.Smith';
+GO
+EXEC [dbo].[AddBudgetSetting] 
+        @Fk_budgetId = 3,
+		@Fk_userId = 1,
+		@BudgetTrigger = 'Cologne';
+GO
+EXEC [dbo].[AddBudgetSetting] 
         @Fk_budgetId = 4,
 		@Fk_userId = 1,
-		@BudgetTrigger = 'Gautrain';
+		@BudgetTrigger = 'Movies';
+GO
+EXEC [dbo].[AddBudgetSetting] 
+        @Fk_budgetId = 4,
+		@Fk_userId = 1,
+		@BudgetTrigger = 'Go-cart';
+GO
+EXEC [dbo].[AddBudgetSetting] 
+        @Fk_budgetId = 5,
+		@Fk_userId = 2,
+		@BudgetTrigger = 'McDonalds';
+GO
+EXEC [dbo].[AddBudgetSetting] 
+        @Fk_budgetId = 5,
+		@Fk_userId = 2,
+		@BudgetTrigger = 'KFC';
+GO
+EXEC [dbo].[AddBudgetSetting] 
+        @Fk_budgetId = 6,
+		@Fk_userId = 2,
+		@BudgetTrigger = 'Tissue';
+GO
+		
 
-SELECT * FROM BudgetSetting;
+
+
