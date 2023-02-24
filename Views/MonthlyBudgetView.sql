d585147bd3fcec606a03937ecba6d08f905602dd
Mmasehume Raphiri <mmasehume@bbd.co.za>
2023-02-24 04:33:51 +0200

    temp monthly view

---
+++ Views/MonthlyBudgetView.sql
@@ -0,0 +1,11 @@
+USE BudgetTrackingDB;
+GO
+
+DROP VIEW IF EXISTS [dbo].[MonthlyBudget];
+GO
+
+CREATE VIEW MonthlyBudget AS
+SELECT budgetName, budgetLimit
+FROM Budget INNER JOIN Periods 
+ON Budget.fk_periodId = Periods.periodId
+WHERE MONTH(Periods.startDate) = 3
