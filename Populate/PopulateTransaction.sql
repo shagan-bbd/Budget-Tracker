ec1c526578b7c2a6139037396ab9552c17eca4b9
Mmasehume Raphiri <mmasehume@bbd.co.za>
2023-02-24 06:25:27 +0200

    fix syntax error

--- Populate/PopulateTransaction.sql
+++ Populate/PopulateTransaction.sql
@@ -14,13 +14,13 @@ EXEC [dbo].[AddTransaction]
         @TransactionAmount = -200,
 		@TransactionDate = '2023-03-10',
 		@TransactionReference= 'Go-carting at the mall',
-			@Deleted = 0;
+		@Deleted = 0;
 GO
 EXEC [dbo].[AddTransaction] 
 		@Fk_budgetId = 5,
 		@Fk_userId = 2, 
         @TransactionAmount = -100,
-		@TransactionDate = '2023-054-08',
+		@TransactionDate = '2023-04-08',
 		@TransactionReference= 'BigMac from McDonalds',
 			@Deleted = 0;
 GO
