083452ec836f66dd3cee0961c0209f32b748d2cc
Mmasehume Raphiri <mmasehume@bbd.co.za>
2023-02-24 05:50:08 +0200

    fix syntax error

--- Populate/PopulateBudget.sql
+++ Populate/PopulateBudget.sql
@@ -2,7 +2,7 @@ USE BudgetTrackingDB
 GO
 
 EXEC [dbo].[AddBudget] @fk_userId = 1, @fk_periodId = 1, @budgetLimit = 500.00, @budgetName = 'Groceries';
-EXEC [dbo].[AddBudget] @fk_userId = , @fk_periodId = 1, @budgetLimit = 1500.00, @budgetName = 'Rent';
+EXEC [dbo].[AddBudget] @fk_userId = 1, @fk_periodId = 1, @budgetLimit = 1500.00, @budgetName = 'Rent';
 EXEC [dbo].[AddBudget] @fk_userId = 1, @fk_periodId = 1, @budgetLimit = 100.00, @budgetName = 'Personal Care';
 EXEC [dbo].[AddBudget] @fk_userId = 1, @fk_periodId = 1, @budgetLimit = 200.00, @budgetName = 'Entertainment';
 GO
